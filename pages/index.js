97738965bf0799b1175413325b910ad07c055aa1
DiegoGustavoRamirez <diegormz111@gmail.com>
2021-04-07 22:14:32 -0600

    splashscreen implemented

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
+import {
+  useEffect
+} from 'react';
+import { useRouter } from 'next/router';
+
 import ContainerFullscreen from '../src/components/container/fullscreen';
 import Logo from '../src/components/icon/logo';
 import Head from '../src/components/head/index';
 
-const Index = () => (
-  <ContainerFullscreen>
-    <Head />
-    <Logo />
-  </ContainerFullscreen>
-);
+const Index = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+      const timeoutReference = setTimeout(function () {
+          router.push(`/home`);
+      }, 2000);
+
+    return () => clearTimeout(timeoutReference);
+  }, []);
+
+  return (
+      <ContainerFullscreen>
+        <Head />
+        <Logo />
+      </ContainerFullscreen>
+  );
+};
 
 export default Index;
