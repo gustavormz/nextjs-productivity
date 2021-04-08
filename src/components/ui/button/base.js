2d8c7b1c496c524681a384d7175f9b82d3b9e2a9
DiegoGustavoRamirez <diegormz111@gmail.com>
2021-04-07 23:41:33 -0600

    created button base and secondary; textfiled base all without complete styling just the basics

---
+++ src/components/ui/button/base.js
@@ -0,0 +1,26 @@
+import {
+    Button,
+    withStyles
+} from '@material-ui/core';
+
+const styles = theme => ({
+    root: {
+        width: `100%`,
+        fontSize: `0.9rem`,
+        paddingTop: `6px`,
+        paddingBottom: `6px`,
+        [theme.breakpoints.down('md')]: {
+            fontSize: `0.85rem`
+        },
+        [theme.breakpoints.down('sm')]: {
+            fontSize: `0.75rem`
+        }
+    },
+    disabled: {
+        backgroundColor: `#d3d3d3`
+    }
+});
+
+const ButtonBase = withStyles(styles)(Button);
+
+export default ButtonBase;
