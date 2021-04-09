2c1267ce1fb60dc347242943a7ad155fb26b4137
DiegoGustavoRamirez <diegormz111@gmail.com>
2021-04-08 19:54:36 -0600

    added all necessary dialogs to create, update, delete task

--- pages/home.js
+++ pages/home.js
@@ -8,27 +8,287 @@ import Head from '../src/components/head/home';
 import LayoutBase from '../src/components/layout/base';
 import Page from '../src/pages/home/page';
 import Static from '../src/pages/home/static';
+import DialogTaskCreate from '../src/components/dialog/task/create';
+import DialogLoaderFullscreenCircular from '../src/components/dialog/loader/fullscreenCircular';
+import DialogResponse from '../src/components/dialog/response';
+import DialogTaskEdit from '../src/components/dialog/task/edit';
+import DialogTaskDeleteConfirm from '../src/components/dialog/task/delete';
+
+const removeTaskByIdFromArray = (_tasks, idTask) => {
+    let indexToDelete = -1;
+    const tasks = [..._tasks];
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].timestamp === idTask) {
+            indexToDelete = i;
+            break;
+        }
+    }
+    tasks.splice(indexToDelete, 1);
+    return tasks;
+};
+
+const updateTaskByIdFromArray = (_tasks, idTask, newData) => {
+    const tasks = [..._tasks];
+    for (let i; i < tasks.length; i++) {
+        if (tasks[i].timestamp === idTask) {
+            tasks[i] = { ...newData };
+            break;
+        }
+    }
+    return tasks;
+};
 
 const Home = ({
-    endpoint
+    baseApiUrl
 }) => {
+    const [state, setState] = useState({
+        isRequesting: false,
+        isDialogTaskCreateOpen: false,
+        newTask: undefined,
+        dialogResponseType: `default`,
+        isError: false,
+        isDialogResponseOpen: false,
+        updateTask: undefined,
+        isDialogTaskEditOpen: false,
+        isDialogTaskDeleteConfirmOpen: false,
+        deleteTask: undefined
+    });
     const [tasks, setTasks] = useState(undefined);
+    const [durations, setDurations] = useState([])
     
     useEffect(() => {
-        async function getAllTasks () {
-            const response = await (await fetch(endpoint)).json();
-            console.log(response);
-            const taskToSet = response.error ? [] : response.data.Items;
-            setTasks(taskToSet);
+        async function fetchTaskAndDurations () {
+            const responses = await Promise.all([
+                await fetch(`${baseApiUrl}/task`),
+                await fetch(`${baseApiUrl}/duration`)
+            ]);
+
+            const tasksToSet = await (responses[0]).json();
+            const durationsToSet = await (responses[1]).json();
+            durationsToSet.data.push({
+                label: `Personalizar`,
+                value: `custom`
+            });
+
+            setTasks(tasksToSet.data);
+            setDurations(durationsToSet.data);
         }
-        getAllTasks();
+        fetchTaskAndDurations();
     }, []);
+
+    function handleDialogTaskCreateClose () {
+        setState({
+            ...state,
+            isDialogTaskCreateOpen: false
+        });
+    }
+
+    function handleNewTaskButtonClick () {
+        setState({
+            ...state,
+            isDialogTaskCreateOpen: true
+        });
+    }
+
+    async function handleDialogTaskCreateFormSubmit (values) {
+        setState({
+            ...state,
+            isRequesting: true,
+            newTask: values
+        });
+
+        let dialogResponseType = undefined;
+        let durationResponse = undefined;
+        let isError = false;
+
+        if (values.duration === `custom`) {
+            durationResponse = await (await fetch(`${baseApiUrl}/duration`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    ...values,
+                    label: values.duration_title
+                })
+            })).json();
+
+            if (durationResponse.error) {
+                dialogResponseType = durationResponse.errorType;
+                isError = true;
+            } else {
+                const durationsCopy = [...durations];
+                durationsCopy.push(durationResponse.data);
+                setDurations(durationsCopy);
+
+                const response = await (await fetch (`${baseApiUrl}/task`, {
+                    method: `POST`,
+                    body: JSON.stringify(values)
+                })).json();
+        
+                if (!response.error) {
+                    const tasksCopy = [...tasks];
+                    tasksCopy.push(response.data);
+                    setTasks(tasksCopy);
+                }
+                isError = response.error;
+                dialogResponseType = response.errorType || response.successType
+            }
+        } else {
+            const response = await (await fetch (`${baseApiUrl}/task`, {
+                method: `POST`,
+                body: JSON.stringify(values)
+            })).json();
+    
+            if (!response.error) {
+                const tasksCopy = [...tasks];
+                tasksCopy.push(response.data);
+                setTasks(tasksCopy);
+            }
+
+            isError = response.error;
+            dialogResponseType = response.errorType || response.successType;
+        }
+
+        setState({
+            ...state,
+            isRequesting: false,
+            isError,
+            dialogResponseType,
+            isDialogResponseOpen: true,
+            isDialogTaskCreateOpen: isError,
+            newTask: isError ?
+                values:
+                undefined
+        });
+    }
+
+    function handleDialogResponseClose () {
+        setState({
+            ...state,
+            isDialogResponseOpen: false
+        });
+    }
+
+    function handleEditClick (task) {
+        console.log(task);
+        setState({
+            ...state,
+            updateTask: task,
+            isDialogTaskEditOpen: true
+        });
+    }
+
+    function handleDialogTaskEditClose () {
+        setState({
+            ...state,
+            isDialogTaskEditOpen: false
+        });
+    }
+
+    async function handleDialogTaskEditFormSubmit (values) {
+        setState({
+            ...state,
+            isRequesting: true,
+            updateTask: values
+        });
+
+        const response = await (await fetch (`${baseApiUrl}/task/${values.timestamp}`, {
+            method: `PUT`,
+            body: JSON.stringify(values)
+        })).json();
+
+        console.log(response);
+
+        // if there isnt error update the item on the array
+        if (!response.error) {
+            const newTasks = updateTaskByIdFromArray(tasks, values.timestamp, values);
+            setTasks(newTasks);
+        }
+
+        setState({
+            ...state,
+            isRequesting: false,
+            isError: response.error,
+            dialogResponseType: response.errorType || response.successType,
+            isDialogResponseOpen: true,
+            isDialogTaskEditOpen: response.error,
+            updateTask: response.error ?
+                values :
+                undefined
+        });
+    }
+
+    function handleDeleteClick (task) {
+        setState({
+            ...state,
+            isDialogTaskDeleteConfirmOpen: true,
+            deleteTask: task
+        });
+    }
+
+    async function handleDialogTaskDeleteConfirm () {
+        setState({
+            ...state,
+            isRequesting: true
+        });
+
+        const response = await (await fetch(`${baseApiUrl}/task/${state.deleteTask.timestamp}`, {
+            method: `DELETE`
+        })).json();
+
+        if (!response.error) {
+            const newTasks = removeTaskByIdFromArray(tasks, state.deleteTask.timestamp);
+            setTasks(newTasks);
+        }
+
+        setState({
+            ...state,
+            isDialogTaskDeleteConfirmOpen: response.error,
+            deleteTask: response.error ?
+                state.deleteTask :
+                undefined
+        });
+    }
+
+    function handleDialogTaskDeleteClose () {
+        setState({
+            ...state,
+            isDialogTaskDeleteConfirmOpen: false
+        });
+    }
 
     return (
         <LayoutBase>
             <Head />
+            { state.deleteTask && (
+                <DialogTaskDeleteConfirm
+                    handleClose={handleDialogTaskDeleteClose}
+                    handleConfirm={handleDialogTaskDeleteConfirm}
+                    id={state.deleteTask.timestamp}
+                    open={state.isDialogTaskDeleteConfirmOpen}
+                    title={state.deleteTask.title}/>
+            ) }
+            <DialogResponse
+                handleClose={handleDialogResponseClose}
+                open={state.isDialogResponseOpen}
+                type={state.dialogResponseType}/>
+            <DialogLoaderFullscreenCircular open={state.isRequesting}/>
+            <DialogTaskCreate
+                durations={durations}
+                finalValues={state.newTask}
+                handleClose={handleDialogTaskCreateClose}
+                handleFormSubmit={handleDialogTaskCreateFormSubmit}
+                open={state.isDialogTaskCreateOpen}/>
+            <DialogTaskEdit
+                durations={durations}
+                finalValues={state.updateTask}
+                handleClose={handleDialogTaskEditClose}
+                handleFormSubmit={handleDialogTaskEditFormSubmit}
+                open={state.isDialogTaskEditOpen}/>
             { tasks ? (
                 <Page
+                    handleDeleteClick={handleDeleteClick}
+                    durations={durations}
+                    handleEditClick={handleEditClick}
+                    handleNewTaskButtonClick={handleNewTaskButtonClick}
                     tasks={tasks}/>
             ) : (
                 <Static />
@@ -38,17 +298,17 @@ const Home = ({
 };
 
 export const getServerSideProps = () => {
-    const endpoint = `/api/task`;
+    const baseApiUrl = `/api`;
 
     return {
         props: {
-            endpoint
+            baseApiUrl
         }
     };
 };
 
 Home.propTypes = {
-    endpoint: PropTypes.string
+    baseApiUrl: PropTypes.string
 };
 
 export default Home;
