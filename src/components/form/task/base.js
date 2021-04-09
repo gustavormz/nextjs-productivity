4189b4c3a29135aec62a099388ac90a23f5ae872
DiegoGustavoRamirez <diegormz111@gmail.com>
2021-04-08 19:52:11 -0600

    base taks form updated and adpted to the new task model

--- src/components/form/task/base.js
+++ src/components/form/task/base.js
@@ -4,6 +4,11 @@ import {
 } from '@material-ui/core';
 
 import TextFieldBase from '../../ui/textField/base';
+import SelectTaskStatus from '../../ui/select/task/status';
+import SelectTaskDuration from '../../ui/select/task/duration';
+import TypographyBase from '../../typography/base';
+import FormDuration from '../../form/duration';
+import SelectBase from '../../ui/select/base';
 
 const isTextFieldError = (errors, name, touched) => 
     errors.hasOwnProperty(name) && touched[name];
@@ -16,7 +21,8 @@ const FormTaskBase = ({
     handleChange,
     handleBlur,
     errors,
-    touched
+    touched,
+    isStatusEdit
 }) => (
     <Grid container spacing={2}>
         <Grid item xs={12}>
@@ -43,17 +49,52 @@ const FormTaskBase = ({
                 multiline={true}
                 rows={2}/>
         </Grid>
+        <Grid item xs={12}>
+            <SelectTaskStatus
+                disabled={!isEdit || !isStatusEdit}
+                onChange={handleChange ? handleChange(`status`) : null}
+                value={values.status}/>
+        </Grid>
+        <Grid item xs={12}>
+            <TypographyBase>
+                Duración
+            </TypographyBase>
+        </Grid>
+        <Grid item xs={12}>
+            <SelectBase
+                labelWidth={62}
+                inputProps={{
+                    name: 'duration'
+                }}
+                title={`Duración`}
+                disabled={!isEdit}
+                onChange={handleChange ? handleChange(`duration`) : null}
+                value={values.duration}
+                options={values.durations}/>
+        </Grid>
+        { values.duration === `custom` && (
+            <Grid item xs={12}>
+                <FormDuration
+                    values={values}
+                    isEdit={isEdit}
+                    handleChange={handleChange}
+                    handleBlur={handleBlur}
+                    errors={errors}
+                    touched={touched}/>
+            </Grid>
+        ) }
     </Grid>
 );
 
 FormTaskBase.propTypes = {
     values: PropTypes.shape({
-        duration: PropTypes.number,
         title: PropTypes.string,
         description: PropTypes.string,
         index: PropTypes.number,
         status: PropTypes.string,
-        timestamp: PropTypes.number
+        timestamp: PropTypes.number,
+        duration_title: PropTypes.string,
+        durations: PropTypes.array
     }),
     isEdit: PropTypes.bool,
     handleBlur: PropTypes.func,
@@ -61,17 +102,15 @@ FormTaskBase.propTypes = {
     errors: PropTypes.object,
     touched: PropTypes.object,
     setFieldValue: PropTypes.func,
-    mapStatus: PropTypes.object
+    mapStatus: PropTypes.object,
+    isStatusEdit: PropTypes.bool
 };
 
 FormTaskBase.defaultProps = {
     isEdit: true,
     errors: {},
     touched: {},
-    mapStatus: {
-        'PENDING': `Pendiente`,
-        'FINISHED': `Terminada` 
-    }
+    isStatusEdit: true
 };
 
 export default FormTaskBase;
