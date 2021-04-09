c5657896bb5c3a6d5244940a6ca474a4099b3e49
DiegoGustavoRamirez <diegormz111@gmail.com>
2021-04-08 19:41:08 -0600

    added api files for duration entity. Functions implemented: create

---
+++ pages/api/duration/index.js
@@ -0,0 +1,112 @@
+import initialDurations from './durations.json';
+import db from '../../../lib/db';
+import validation from '../../../lib/validation/duration';
+import utils from '../../../lib/utils';
+
+const formatTime = ({
+    minutes,
+    seconds
+}) => `(${minutes} minutos${seconds ? `, ${seconds} segundos` : ``})`;
+
+const formatDurationCreateData = data => {
+    const timestamp = data.timestamp || Date.now();
+    return {
+        ...data,
+        timestamp,
+        type: `duration`,
+        value: timestamp
+    };
+};
+
+const formatDurationsArray = durations => durations.reduce((newArray, duration) => {
+    const durationFormated = {
+        ...duration,
+        label: `${duration.label} ${formatTime(duration)}`
+    };
+    newArray.push(durationFormated);
+    return newArray;
+}, []);
+
+const createDuration = async duration => {
+    try {
+        const requestBodyValidated = validation.validateCreate(formatDurationCreateData(duration));
+        const params = {
+            TableName: process.env.REACT_APP_DYNAMODB_TABLE_NAME,
+            Item: requestBodyValidated
+        };
+
+        await db.put(params).promise();
+    } catch (e) {
+        console.error(`Error creating duration`, e);
+        utils.constructCustomErrorByType('DURATION_CREATE')
+    }
+};
+
+const getDurationsByParams = async _params => {
+    try {
+        const params = {
+            TableName: process.env.REACT_APP_DYNAMODB_TABLE_NAME,
+            ..._params
+        };
+
+        return await db.query(params).promise();
+    } catch (e) {
+        console.error(`Error getting durations by query`, e);
+        utils.constructCustomErrorByType();
+    }
+};
+
+const handler = async ({
+    method,
+    body
+}, res) => {
+    let statusResponseCode = 403; // forbbiden as default status code
+    let response = ``;
+
+    try {
+        if (method === 'GET') { // get all durations
+            const KeyConditionExpression = "#type = :type";
+            const ExpressionAttributeNames = {
+                "#type": "type"
+            };
+            const ExpressionAttributeValues = {
+                ":type": "duration"
+            };
+
+            let dynamoResponse = await getDurationsByParams({
+                KeyConditionExpression,
+                ExpressionAttributeNames,
+                ExpressionAttributeValues
+            });
+
+            // add default durations if doesnt exist durations on database
+            if (!dynamoResponse.Items || dynamoResponse.Items.length === 0) {
+                await Promise.all(initialDurations.map(async duration => {
+                    await createDuration(duration);
+                }));
+                dynamoResponse = initialDurations;
+            }
+
+            response = utils.constructSuccessResponse({
+                type: `DURATION_FOUND`,
+                data: formatDurationsArray(dynamoResponse.Items || dynamoResponse)
+            });
+            statusResponseCode = 200;
+        } else if (method === 'POST') { //create duration
+            //validate request body
+            const bodyObject = JSON.parse(body);
+            const requestBodyValidated = await createDuration(bodyObject);
+
+            response = utils.constructSuccessResponse({
+                type: `DURATION_CREATED`,
+                data: requestBodyValidated
+            });
+            statusResponseCode = 201;
+        }
+    } catch (e) {
+        response = utils.constructErrorResponse(e);
+    }
+    res.status(statusResponseCode).json(response);
+};
+
+export default handler;
