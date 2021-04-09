04c19174fb432bf9ec7b9f1f03c919e4d5d2070e
DiegoGustavoRamirez <diegormz111@gmail.com>
2021-04-08 19:53:32 -0600

    no changes

--- src/components/card/task.js
+++ src/components/card/task.js
@@ -4,30 +4,43 @@ import {
     CardActions,
     CardContent,
     CardHeader,
-    Avatar
+    Avatar,
+    IconButton
 } from '@material-ui/core';
+import {
+    Edit,
+    Cancel
+} from '@material-ui/icons';
 
 import TypographyBase from '../typography/base';
 
 const CardTask = ({
-    duration,
-    title,
-    description,
-    index,
-    status,
-    timestamp
+    task,
+    handleEditClick,
+    handleDeleteClick,
+    durations
 }) => (
     <Card variant={'outlined'}>
         <CardHeader
+            action={
+                <>
+                    <IconButton onClick={() => handleEditClick(task)}>
+                        <Edit />
+                    </IconButton>
+                    <IconButton onClick={() => handleDeleteClick(task)}>
+                        <Cancel />
+                    </IconButton>
+                </>
+            }
             avatar={
                 <Avatar>
-                    {title[0]}        
+                    {task.title[0]}        
                 </Avatar>
             }
-            title={title}/>
+            title={task.title}/>
         <CardContent>
             <TypographyBase style={{ textAlign: 'justify' }}>
-                {description}
+                {task.description}
             </TypographyBase>
         </CardContent>
         <CardActions>
@@ -37,12 +50,32 @@ const CardTask = ({
 );
 
 CardTask.propTypes = {
-    duration: PropTypes.number,
-    title: PropTypes.string,
-    description: PropTypes.string,
-    index: PropTypes.number,
-    status: PropTypes.string,
-    timestamp: PropTypes.number
+    task: PropTypes.shape({
+        duration: PropTypes.number,
+        title: PropTypes.string,
+        description: PropTypes.string,
+        index: PropTypes.number,
+        status: PropTypes.string,
+        timestamp: PropTypes.number,
+        minutes: PropTypes.number,
+        seconds: PropTypes.seconds
+    }),
+    handleEditClick: PropTypes.func,
+    handleDeleteClick: PropTypes.func,
+    durations: PropTypes.array
+};
+
+CardTask.defaultProps = {
+    task: {
+        duration: 0,
+        title: ``,
+        description: ``,
+        index: 0,
+        status: `PENDING`,
+        timestamp: 0,
+        minutes: 0,
+        seconds: 0
+    }
 };
 
 export default CardTask;
